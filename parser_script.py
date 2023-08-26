54f76768a1f07f2211fd25d3be40dc1d381fd4e9
geekingzone <geekingzone@protonmail.com>
2023-08-26 12:32:16 +0200

    feat: testing to create files

--- parser_script.py
+++ parser_script.py
@@ -7,14 +7,22 @@ podcast_rss_url = "https://anchor.fm/s/5879dae4/podcast/rss"
 def main():
     # Obtener el directorio actual
     current_directory = os.getcwd()
-    print("Directorio actual:", current_directory)
-    
+
     feed = feedparser.parse(podcast_rss_url)
     
     # Aquí puedes procesar la información del feed como desees
     # Por ejemplo, imprimir los títulos de los episodios recientes
     for entry in feed.entries:
-        print(entry.title)
-
+        # Obtener las primeras 5 palabras del título
+        words = entry.title.split()[:5]
+        
+        # Crear el nombre del archivo sustituyendo espacios por guiones
+        file_name = "-".join(words) + ".md"
+        
+        # Crear y escribir en el archivo temporal
+        with open(os.path.join(current_directory, file_name), "w") as file:
+            file.write(entry.title)
+            print(f"Archivo '{file_name}' creado.")
+    
 if __name__ == "__main__":
     main()
