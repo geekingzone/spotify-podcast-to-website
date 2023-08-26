380211b0193370090fccbe0959837c7d0ed5c99a
geekingzone <geekingzone@protonmail.com>
2023-08-26 12:36:22 +0200

    feat: remove special characters

--- parser_script.py
+++ parser_script.py
@@ -1,8 +1,15 @@
 import os
 import feedparser
+import unicodedata
 
 # URL del RSS del podcast
 podcast_rss_url = "https://anchor.fm/s/5879dae4/podcast/rss"
+
+def remove_special_characters(text):
+    # Normalizar el texto y eliminar caracteres especiales
+    normalized_text = unicodedata.normalize("NFKD", text)
+    cleaned_text = ''.join([c for c in normalized_text if not unicodedata.combining(c)])
+    return cleaned_text
 
 def main():
     # Obtener el directorio actual
@@ -18,6 +25,9 @@ def main():
         
         # Crear el nombre del archivo sustituyendo espacios por guiones
         file_name = "-".join(words) + ".md"
+        
+        # Eliminar caracteres especiales del nombre del archivo
+        clean_file_name_prefix = remove_special_characters(file_name_prefix)
         
         # Crear y escribir en el archivo temporal
         with open(os.path.join(current_directory, file_name), "w") as file:
